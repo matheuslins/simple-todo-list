4fe412407adfb5d05682236f5760b51360f4c382
Matheus Lins <msl@cin.ufpe.br>
2018-07-15 23:08:59 -0300

    Config basica do server

---
+++ src/config/server.js
@@ -0,0 +1,15 @@
+const port = 3003
+const bodyParser = require('body-parser')
+const express = require('express')
+const server = express()
+
+// Midleweres
+
+/* Captura todas as requisicoes que usam o padrao urlencoded (padrao usado na
+ submisao de formularios) para que o bodyParser faca o parser. */
+server.use(bodyParser.urlencoded({extended:true}))
+server.use(bodyParser.json())
+
+server.listen(port, function() {
+    console.log(`Bakend is running on port ${port}.`)
+})
