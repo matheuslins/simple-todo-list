f6724e89bcb5f66c7c7b6574de4330575a4e034a
Matheus Lins <msl@cin.ufpe.br>
2018-07-17 11:11:59 -0300

    Faz o bind na funcao handleAdd para o this ser a instancia da classe

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -4,8 +4,12 @@ import TodoForm from './todoForm'
 import TodoList from './todoList'
 
 export default class Todo extends Component {
+    constructor(props){
+        super(props)
+        this.handleAdd = this.handleAdd.bind(this)
+    }
     handleAdd(){
-        console.log('Add')
+        console.log(this)
     }
     render(){
         return (
