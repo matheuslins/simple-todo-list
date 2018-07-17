4583f28a76b745d3441f6312284c5884ae3c0027
Matheus Lins <msl@cin.ufpe.br>
2018-07-16 22:07:12 -0300

    Cria as rotas do menu

--- frontend/src/template/menu.jsx
+++ frontend/src/template/menu.jsx
@@ -8,13 +8,12 @@ export default props => (
                     <i className="fa fa-calendar-check-o"></i> TodoApp
                 </a>
             </div>
-        </div>
-
-        <div id="navbar" className="navbar-collapse collapse">
-            <ul className="nav navbar-nav">
-                <li><a href="#/todos">Taferas</a></li>
-                <li><a href="#/about">Sobre</a></li>
-            </ul>
+            <div id="navbar" className="navbar-collapse collapse">
+                <ul className="nav navbar-nav">
+                    <li><a href="#/todos">Taferas</a></li>
+                    <li><a href="#/about">Sobre</a></li>
+                </ul>
+            </div>
         </div>
     </nav>
 )
